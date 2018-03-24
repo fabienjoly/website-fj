c0edbef3d2f591c922c7e9efec14c2a979261672
AluneCM <hello@alune.fr>
2018-03-24 16:58:48 +0100

    button style stuff

--- _site/assets/js/ui.js
+++ _site/assets/js/ui.js
@@ -36,3 +36,50 @@ var pageBody = document.body;
 menuContainer.addEventListener("transitionend", OnTransitionEnd, false);
 menuToggler.addEventListener("click", toggleClassMenu, false);
 menuCache.addEventListener("click", toggleClassMenu, false);
+
+
+// Buttons highlight stuff
+
+var docButtons = document.getElementsByClassName("project-detail-button");
+
+function getPosition(e) {
+  var rect = e.target.getBoundingClientRect();
+  var x = e.clientX - rect.left;
+  var y = e.clientY - rect.top;
+  return {
+    x,
+    y
+  }
+}
+
+
+function setHighlightPosition(e) {
+	var position = getPosition(e);
+	console.log(position);
+	var createHighlight = document.createElement("span");
+	createHighlight.classList.add("button-click-highlight");
+	createHighlight.style.top = position.y - 15 + "px";
+	createHighlight.style.left = position.x - 15 + "px";
+	this.appendChild(createHighlight);
+
+	setTimeout(function(){
+		createHighlight.remove();
+  }, 600);
+
+
+	// buttonHighlight.classList.add("animated");
+  // setTimeout(function(){
+  //   buttonHighlight.classList.remove("animated");
+  // }, 600);
+}
+
+for (var i = 0; i < docButtons.length; i++) {
+	docButtons[i].addEventListener("click", setHighlightPosition, false);
+}
+
+
+// for (var i = 0; i < docButtons.length; i++) {
+// 	docButtons[i].addEventListener("click", function() {
+// 		console.log("button clicked man !");
+// 	}, false);
+// }
