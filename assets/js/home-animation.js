d67e7f96b9e42d166a48b9e7a465b2afe46e2ea7
AluneCM <hello@alune.fr>
2018-06-02 17:02:59 +0200

    added home animation on test page /home-animation-test

---
+++ assets/js/home-animation.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('lottie-animation'),
+  renderer: 'svg',
+  loop: false,
+  autoplay: true,
+  path: 'assets/js/home-animation.json'
+})
